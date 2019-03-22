6415a5ffc1fbf6bd08863334850c484031bef8ae
Behibrogit <behibro.akissi@gmail.com>
2019-03-22 11:33:08 +0000

    Ajouté la table Agent

--- model.py
+++ model.py
@@ -1,3 +1,12 @@
 import json
 import math
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+
+# creation de table
+class Agent:
+    def say_hello(self, first_name):
+        return "Bien le Bonjour" + first_name+" !"
+
+    def __init__(self, **agent_attributes):
+        for attr_name, attr_value in agent_attributes.items():
+            setattr(self,attr_name, attr_value
